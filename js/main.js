dd4319d79946b8482a2a2a0b9cdebbc19c59f285
MarcosLima33 <antoniomarcos.lima33@gmail.com>
2021-07-08 12:06:34 -0300

    aula js 90%

---
+++ js/main.js
@@ -0,0 +1,71 @@
+//var nome = "Antonio Marcos";
+//var idade = 30;
+//var idade2 = 10;
+//var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(frase.replace("Japão", "Brasil"));
+
+
+//var lista = ["maçã", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop();
+//console.log (lista);
+//console.log (lista.toString());
+//console.log (lista.join(" - "));
+
+
+//var fruta = {nome:"maçã", cor:"vermelha"}
+//console.log(fruta.nome);
+
+//var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+//console.log(frutas[0].nome);
+//var idade = prompt("Qual a sua idade? ");
+//var idade = 18;
+//if (idade >= 18){
+//   alert("maior de idade");
+//}else{
+//    alert("menor de idade");
+//}
+
+//var count = 0;
+//while(count <= 5){
+//    console.log(count);
+//    count = count + 1;
+//}
+
+//var count;
+
+//for(count=0; count <=5; count++){
+//    alert(count);
+//}
+
+
+//var d = new Date();
+
+//alert(d);
+
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+function setReplace(frase, nome, novo_nome){
+  return  frase.replace(nome, novo_nome)
+}
+
+function validaIdade(idade){
+    var validar;
+    if (idade >= 18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt("Qual sua idade");
+console.log(validaIdade(idade));
+
+//alert(soma(5, 10));
+//alert(setReplace("Vai japao", "japao", "brasil"));
